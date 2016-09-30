f0818b98deac295c130862f09215393becce9c1a
Dima Bobrov <dm.g.bobrov@gmail.com>
2016-09-30 11:11:54 +0300

    no message

--- ChartsDemo/Classes/DemoBaseViewController.m
+++ ChartsDemo/Classes/DemoBaseViewController.m
@@ -2,6 +2,8 @@
 //  DemoBaseViewController.m
 //  ChartsDemo
 //
+//  Created by Daniel Cohen Gindi on 13/3/15.
+//
 //  Copyright 2015 Daniel Cohen Gindi & Philipp Jahoda
 //  A port of MPAndroidChart for iOS
 //  Licensed under Apache License 2.0
@@ -42,6 +44,11 @@
 - (void)initialize
 {
     self.edgesForExtendedLayout = UIRectEdgeNone;
+    
+    months = @[
+        @"Jan", @"Feb", @"Mar", @"Apr", @"May", @"Jun", @"Jul", @"Aug", @"Sep",
+        @"Oct", @"Nov", @"Dec"
+        ];
     
     parties = @[
         @"Party A", @"Party B", @"Party C", @"Party D", @"Party E", @"Party F",
@@ -106,7 +113,7 @@
     
     if ([key isEqualToString:@"saveToGallery"])
     {
-        UIImageWriteToSavedPhotosAlbum([chartView getChartImageWithTransparent:NO], nil, nil, nil);
+        [chartView saveToCameraRoll];
     }
     
     if ([key isEqualToString:@"togglePinchZoom"])
@@ -123,6 +130,14 @@
         barLineChart.autoScaleMinMaxEnabled = !barLineChart.isAutoScaleMinMaxEnabled;
         
         [chartView notifyDataSetChanged];
+    }
+    
+    if ([key isEqualToString:@"toggleHighlightArrow"])
+    {
+        BarChartView *barChart = (BarChartView *)chartView;
+        barChart.drawHighlightArrowEnabled = !barChart.isDrawHighlightArrowEnabled;
+        
+        [chartView setNeedsDisplay];
     }
     
     if ([key isEqualToString:@"toggleData"])
@@ -262,7 +277,7 @@
     chartView.drawSlicesUnderHoleEnabled = NO;
     chartView.holeRadiusPercent = 0.58;
     chartView.transparentCircleRadiusPercent = 0.61;
-    chartView.chartDescription.enabled = NO;
+    chartView.descriptionText = @"";
     [chartView setExtraOffsetsWithLeft:5.f top:10.f right:5.f bottom:5.f];
     
     chartView.drawCenterTextEnabled = YES;
@@ -292,10 +307,7 @@
     chartView.highlightPerTapEnabled = YES;
     
     ChartLegend *l = chartView.legend;
-    l.horizontalAlignment = ChartLegendHorizontalAlignmentRight;
-    l.verticalAlignment = ChartLegendVerticalAlignmentTop;
-    l.orientation = ChartLegendOrientationVertical;
-    l.drawInside = NO;
+    l.position = ChartLegendPositionRightOfChart;
     l.xEntrySpace = 7.0;
     l.yEntrySpace = 0.0;
     l.yOffset = 0.0;
@@ -303,12 +315,14 @@
 
 - (void)setupRadarChartView:(RadarChartView *)chartView
 {
-    chartView.chartDescription.enabled = NO;
+    chartView.descriptionText = @"";
+    chartView.noDataTextDescription = @"You need to provide data for the chart.";
 }
 
 - (void)setupBarLineChartView:(BarLineChartViewBase *)chartView
 {
-    chartView.chartDescription.enabled = NO;
+    chartView.descriptionText = @"";
+    chartView.noDataTextDescription = @"You need to provide data for the chart.";
     
     chartView.drawGridBackgroundEnabled = NO;
     
